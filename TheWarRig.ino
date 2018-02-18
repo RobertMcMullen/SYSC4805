bb559fe9f4ce59b8da8b1933c4a58ef6a2920cc1
RobertMcMullen <robertmcmullen@cmail.carleton.ca>
2018-02-18 14:34:21 -0500

    Works for straights and turns, doesn't contain any logic yet

--- TheWarRig.ino
+++ TheWarRig.ino
@@ -11,6 +11,8 @@ int FarRight = LOW;
 int Right = LOW;
 int Center = LOW;
 
+bool started = false;
+
 //New IR Sensor Module
 //       _____________________
 //      /     B     A    C     \
@@ -25,6 +27,9 @@ const int pin4A = 4; //middle sensor
 const int pin3C = 5;
 const int pin2E = 6;
 
+const int lowSpeed = 25;//33
+const int medSpeed = 30;//40
+const int fastSpeed = 40;//55
 
 void setup() {
 Serial.begin(9600);
@@ -35,26 +40,81 @@ pinMode(pin4A, INPUT);
 pinMode(pin3C, INPUT);
 pinMode(pin2E, INPUT);
 
+
 }
 
 
 void loop() {
-//FarLeft = digitalRead(pin12D);
+FarLeft = digitalRead(pin12D);
 Left = digitalRead(pin11B);
 Center = digitalRead(pin4A);
 Right = digitalRead(pin3C);
-//FarRight = digitalRead(pin2E);
+FarRight = digitalRead(pin2E);
 
-Drive();
+//Denotes the start of the maze, from this point on, if all the lights go HIGH then it must turn around
+if((started == false) && ((FarRight == LOW) || (Right == LOW) || (Center == LOW) || (Left == LOW) || (FarLeft == LOW))){
+  started = true;
+}
 
-if(Left == LOW){
+//END
+else if((FarRight == LOW) && (Right == LOW) && (Center == LOW) && (Left == LOW) && (FarLeft == LOW)){
+  Brake();  
+  Serial.println("FINISHED");  
+}
+
+//Straight Ahead
+else if((FarLeft == HIGH) && (Left == HIGH) && (Right == HIGH) && (FarRight==HIGH)){
+  Serial.println("1"); 
+  Drive(lowSpeed);
+}
+//Turn around 
+else if((FarLeft == HIGH) && (Left == HIGH) && (Right == HIGH) && (FarRight==HIGH) && (Center == HIGH) && (started == true)){
+  Serial.println("0"); 
+  //turnAround(medSpeed);
+}
+//Hard Right or 90deg. right turn
+else if(Right == LOW && FarRight == LOW){
+  Serial.println("3"); 
   Brake();
-  ShiftLeft();
+  ShiftRight(medSpeed,true);
+  delay(100);
 }
-
-if(Right == LOW){
+//Hard Right or 90deg. right turn
+else if(FarRight == LOW && Center == LOW){//Right == LOW && 
+  Serial.println("4"); 
   Brake();
-  ShiftRight();
+  ShiftRight(medSpeed,false);
+}
+//Easy Right or Small correction Right
+else if(Right == LOW && FarRight == HIGH){
+  Serial.println("2"); 
+  Brake();
+  ShiftRight(fastSpeed,true);
+}
+
+//Needs to over correct left, just the outside caught the tape
+else if((Center == HIGH)&& (Left == HIGH) && (FarLeft==LOW)){
+  Serial.println("5"); 
+  Brake();
+  ShiftLeft(medSpeed,false);
+}
+//Hard Left or 90deg. left
+else if(Left == LOW && FarLeft == LOW){
+  Serial.println("7"); 
+  Brake();
+  ShiftLeft(medSpeed,true);
+}
+//Needs to over correct right, just the outside caught the tape
+else if((Center == HIGH)&& (Right == HIGH) && (FarRight==LOW)){
+  Serial.println("8"); 
+  Brake();
+  ShiftRight(medSpeed,false);
+}
+//Easy Left or Small correction Left
+else if(Left == LOW && FarLeft == HIGH){
+  Serial.println("6"); 
+  Brake();
+  ShiftLeft(fastSpeed,true);
 }
 
 
@@ -63,35 +123,89 @@ if(Right == LOW){
 
 
 
-void Drive(){
-Serial.println("DRIVING");
-LeftMotor->setSpeed(50);
-RightMotor->setSpeed(50);
+void Drive(int speed){
+//Serial.println("DRIVING");
+LeftMotor->setSpeed(speed);
+RightMotor->setSpeed(speed);
 LeftMotor->run(FORWARD);
 RightMotor->run(FORWARD);
 delay(100);
 }
 
-void ShiftLeft(){
-Serial.println("SHIFTING LEFT");
-LeftMotor->setSpeed(70);
-LeftMotor->run(BACKWARD);
+void ShiftLeft(int speed,bool dir){
+//Serial.println("SHIFTING LEFT");
+RightMotor->setSpeed(speed);
+if(dir){
+  RightMotor->run(FORWARD);
+}
+else{
+  LeftMotor->run(BACKWARD);
+  LeftMotor->setSpeed(speed*2);
+}
 delay(200);
 Brake();
 }
 
-void ShiftRight(){
-Serial.println("SHIFTING RIGHT");
-RightMotor->setSpeed(70);
-RightMotor->run(BACKWARD);
+void ShiftRight(int speed,bool dir){
+//Serial.println("SHIFTING RIGHT");
+LeftMotor->setSpeed(speed);
+if(dir){
+  LeftMotor->run(FORWARD);
+}
+else{
+  RightMotor->run(BACKWARD);  
+  RightMotor->setSpeed(speed*2);  
+}
 delay(200);
 Brake();
 }
 
 void Brake(){
-Serial.println("BRAKING");
+//Serial.println("BRAKING");
 LeftMotor->run(RELEASE);
 RightMotor->run(RELEASE);
-
 }
+
+//Turns around CCW
+void turnAround(int speed){
+  for (int i =0; i <200;i++){
+  if(digitalRead(pin4A) == LOW){
+    reverse(speed);
+    break;
+  }
+  RightMotor->run(FORWARD); 
+  LeftMotor->run(BACKWARD);
+  RightMotor->setSpeed(fastSpeed);  
+  LeftMotor->setSpeed(fastSpeed);  
+  delay(10);
+  }
+  Brake();
+}
+void reverse(int speed){
+  while(digitalRead(pin4A) == LOW){
+  RightMotor->run(BACKWARD); 
+  LeftMotor->run(BACKWARD);
+  RightMotor->setSpeed(fastSpeed);  
+  LeftMotor->setSpeed(fastSpeed);  
+  delay(10);
+  }
+  /*
+  while(digitalRead(pin4A) == HIGH){
+  RightMotor->run(FORWARD); 
+  LeftMotor->run(FORWARD);
+  RightMotor->setSpeed(speed*2);  
+  LeftMotor->setSpeed(speed*2);  
+  delay(10);
+  }
+  */
+  
+}
+/*
+void fwdCheck(int inputPin){
+  drive(medSpeed);
+  
+}*/
+
+
+
 
