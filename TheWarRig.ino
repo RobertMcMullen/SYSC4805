87cdade5705f04c2a95f34f89a83972812f7c7cf
RobertMcMullen <robertmcmullen@cmail.carleton.ca>
2018-03-08 21:55:14 -0500

    Functional
    Turns coded into the loop

--- TheWarRig.ino
+++ TheWarRig.ino
@@ -13,6 +13,17 @@ int Center = LOW;
 
 bool started = false;
 
+bool turn1 = false;
+bool turn2 = false;
+bool turn3 = false;
+bool turn4 = false;
+bool turn5 = false;
+bool turn6 = false;
+
+bool finished = false;
+
+
+
 //New IR Sensor Module
 //       _____________________
 //      /     B     A    C     \
@@ -27,9 +38,9 @@ const int pin4A = 4; //middle sensor
 const int pin3C = 5;
 const int pin2E = 6;
 
-const int lowSpeed = 33;//33 --25
-const int medSpeed = 30;//40
-const int fastSpeed = 40;//55
+const int lowSpeed = 35;//33 --25
+const int medSpeed = 40;//40
+const int fastSpeed = 47;//55
 
 void setup() {
 Serial.begin(9600);
@@ -43,7 +54,10 @@ pinMode(pin2E, INPUT);
 
 }
 
-
+/*
+ * Left,Right,Right,Left,Right,Right Follow
+ * 
+ */
 void loop() {
 FarLeft = digitalRead(pin12D);
 Left = digitalRead(pin11B);
@@ -51,45 +65,81 @@ Center = digitalRead(pin4A);
 Right = digitalRead(pin3C);
 FarRight = digitalRead(pin2E);
 
-//Denotes the start of the maze, from this point on, if all the lights go HIGH then it must turn around
-if((started == false) && ((FarRight == LOW) || (Right == LOW) || (Center == LOW) || (Left == LOW) || (FarLeft == LOW))){
-  started = true;
-}
+
 
 //END
-else if((FarRight == LOW) && (Right == LOW) && (Center == LOW) && (Left == LOW) && (FarLeft == LOW)){
-  Brake();  
-  Serial.println("FINISHED");  
+if(((FarRight == LOW) && (Right == LOW) && (Center == LOW) && (Left == LOW)) || ((FarLeft == LOW) && (Right == LOW) && (Center == LOW) && (Left == LOW))){
+  Drive(lowSpeed);
+  if(((FarRight == LOW) && (Right == LOW) && (Center == LOW) && (Left == LOW)) || ((FarLeft == LOW) && (Right == LOW) && (Center == LOW) && (Left == LOW))){
+    Brake();  
+    Serial.println("FINISHED");  
+    finished=true;
+  }
+}
+
+
+else if(turn1 == false && FarLeft == LOW){
+  turn1  = true;
+  turnLeft(medSpeed);
+  Brake();
+  delay(100);
+}
+else if(turn1 == true && turn2 == false && FarRight == LOW){
+  turn2 = true;
+  turnRight(medSpeed);
+  Brake();
+  delay(100);
+}
+
+else if(turn1 == true && turn2 == true && turn3 == false && FarLeft == LOW){
+  turn3 = true;
+  turnLeft(medSpeed);
+  Brake();
+  delay(100);
+}
+else if(turn1 == true && turn2 == true && turn3 == true && turn4 == false  && FarRight == LOW){
+  turn4 = true;
+  turnRight(medSpeed);
+  Brake();
+  delay(100);
+}
+else if(turn1 == true && turn2 == true && turn3 == true && turn4 == true && turn5 == false && FarRight == LOW){
+  turn5 = true;
+  turnRight(medSpeed);
+  Brake();
+  delay(100);
+}
+
+else if((FarLeft == HIGH) && (Left == HIGH) && (Right == HIGH) && (FarRight==HIGH) && (Center == HIGH) && finished == false){
+  
+  Drive(medSpeed);    
+  Brake();
+  delay(100);  
 }
 
 //Straight Ahead
 else if((FarLeft == HIGH) && (Left == HIGH) && (Right == HIGH) && (FarRight==HIGH) && (Center == LOW)){
   Serial.println("1"); 
-  Drive(lowSpeed);
+  if(turn6){
+    Drive(lowSpeed);
+  }
+  else {
+    Drive(medSpeed);
+  }
+  
 }
-//Turn around 
-else if((FarLeft == HIGH) && (Left == HIGH) && (Right == HIGH) && (FarRight==HIGH) && (Center == HIGH) && (started == true)){
-  Serial.println("0"); 
-  turnAround(medSpeed);
-}
-//Hard Right or 90deg. right turn
-else if(Right == LOW && FarRight == LOW){
-  Serial.println("3"); 
+//Easy Left or Small correction Left
+else if(Left == LOW && FarLeft == HIGH){
+  Serial.println("6"); 
   Brake();
-  ShiftRight(medSpeed,true);
-  delay(100);
+  ShiftLeft(medSpeed,true);
 }
-//Hard Right or 90deg. right turn
-else if(FarRight == LOW && Center == LOW){//Right == LOW && 
-  Serial.println("4"); 
-  Brake();
-  ShiftRight(medSpeed,false);
-}
+
 //Easy Right or Small correction Right
 else if(Right == LOW && FarRight == HIGH){
   Serial.println("2"); 
   Brake();
-  ShiftRight(fastSpeed,true);
+  ShiftRight(medSpeed,true);
 }
 
 //Needs to over correct left, just the outside caught the tape
@@ -110,12 +160,7 @@ else if((Center == HIGH)&& (Right == HIGH) && (FarRight==LOW)){
   Brake();
   ShiftRight(medSpeed,false);
 }
-//Easy Left or Small correction Left
-else if(Left == LOW && FarLeft == HIGH){
-  Serial.println("6"); 
-  Brake();
-  ShiftLeft(fastSpeed,true);
-}
+
 
 //Easy Left or Small correction Left
 else if(Left == LOW && FarLeft == LOW){
@@ -130,8 +175,53 @@ else if(FarLeft == LOW && Left== HIGH && Center == LOW && Right == HIGH){
   Brake();
   Drive(lowSpeed);
 }
-
+//Hard Right or 90deg. right turn
+else if(Right == LOW && FarRight == LOW){
+  Serial.println("3"); 
+  Brake();
+  ShiftRight(medSpeed,true);
+  delay(100);
 }
+//Hard Right or 90deg. right turn
+else if(FarRight == LOW && Center == LOW){//Right == LOW && 
+  Serial.println("4"); 
+  Brake();
+  ShiftRight(medSpeed,false);
+}
+}//end loop
+
+
+
+
+void maintain(){
+
+  
+FarLeft = digitalRead(pin12D);
+Left = digitalRead(pin11B);
+Center = digitalRead(pin4A);
+Right = digitalRead(pin3C);
+FarRight = digitalRead(pin2E);
+
+
+//Straight Ahead
+if((FarLeft == HIGH) && (Left == HIGH) && (Right == HIGH) && (FarRight==HIGH) && (Center == LOW)){
+  Serial.println("1"); 
+  Drive(lowSpeed);
+}
+//Easy Right or Small correction Right
+else if(Right == LOW && FarRight == HIGH){
+  Serial.println("2"); 
+  Brake();
+  ShiftRight(fastSpeed,true);
+}
+//Easy Left or Small correction Left
+else if(Left == LOW && FarLeft == HIGH){
+  Serial.println("6"); 
+  Brake();
+  ShiftLeft(fastSpeed,true);
+}
+
+}//end loop
 
 
 
@@ -172,51 +262,42 @@ delay(200);
 Brake();
 }
 
+
 void Brake(){
 //Serial.println("BRAKING");
 LeftMotor->run(RELEASE);
 RightMotor->run(RELEASE);
 }
 
-//Turns around CCW
-void turnAround(int speed){
-  for (int i =0; i <200;i++){
+void turnLeft(int speed){
+//Serial.println("SHIFTING LEFT");
+while(true){
+  RightMotor->setSpeed(speed);
+  RightMotor->run(FORWARD);
+  delay(100);
   if(digitalRead(pin4A) == LOW){
-    reverse(speed);
     break;
   }
-  RightMotor->run(FORWARD); 
-  LeftMotor->run(BACKWARD);
-  RightMotor->setSpeed(fastSpeed);  
-  LeftMotor->setSpeed(fastSpeed);  
-  delay(10);
-  }
-  Brake();
 }
-void reverse(int speed){
-  while(digitalRead(pin4A) == LOW){
-  RightMotor->run(BACKWARD); 
-  LeftMotor->run(BACKWARD);
-  RightMotor->setSpeed(fastSpeed);  
-  LeftMotor->setSpeed(fastSpeed);  
-  delay(10);
-  }
-  /*
-  while(digitalRead(pin4A) == HIGH){
-  RightMotor->run(FORWARD); 
+delay(200);
+Brake();
+}
+
+
+void turnRight(int speed){
+//Serial.println("SHIFTING LEFT");
+while(true){
+  LeftMotor->setSpeed(speed);
   LeftMotor->run(FORWARD);
-  RightMotor->setSpeed(speed*2);  
-  LeftMotor->setSpeed(speed*2);  
-  delay(10);
+  delay(100);
+  if(digitalRead(pin4A) == LOW){
+    break;
   }
-  */
-  
 }
-/*
-void fwdCheck(int inputPin){
-  drive(medSpeed);
-  
-}*/
+delay(200);
+Brake();
+}
+
 
 
 
