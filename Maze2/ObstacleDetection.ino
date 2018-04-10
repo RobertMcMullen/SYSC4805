94861f2cac85321c9535fd806b71888ed9542262
AdsBerger <Adam.Berg14@hotmail.com>
2018-04-09 21:01:18 -0400

    Update ObstacleDetection.ino

--- Maze2/ObstacleDetection.ino
+++ Maze2/ObstacleDetection.ino
@@ -1,3 +1,4 @@
+//NECESSARY LIBRARIES FOR MOTOR SHIELD
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
@@ -5,24 +6,21 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *LeftMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *RightMotor = AFMS.getMotor(4);
 
-
-bool turn1 = true;
-bool turn2 = false;
-bool turn3 = false;
-
-
+int IR_Pin = LOW;
 long duration;
 float distance;
 
+//IR SENSOR FOR LEFT WALL DETECTION
 int wallGap = LOW;
 
 const int trigPin_10 = 10;
 const int echoPin_9 = 9;
 const int leftWallPin_12 = 12;
 
-const int lowSpeed = 35;//33 --25
-const int medSpeed = 40;//40
-const int fastSpeed = 47;//55
+//TURNING SPEEDS
+const int lowSpeed = 34;
+const int medSpeed = 40;
+const int fastSpeed = 47;
 
 void setup() {
 pinMode(trigPin_10, OUTPUT); 
@@ -30,61 +28,35 @@ pinMode(echoPin_9, INPUT);
 pinMode(leftWallPin_12,INPUT);
 Serial.begin(9600);
 AFMS.begin();
-
-
-
+int turns = 1;
 }
 
-//Hit wall, Turn Right
-//Hit Wall, Turn right
-//Hit wall turn left
 
 void loop() {
-    long currentDistance = ping();
-    wallGap = digitalRead(leftWallPin_12);
-    /*
-    if(wallGap == HIGH){
-      //This means the robot is closing in on the leftwall and should turn Right
-       ShiftRight(medSpeed,true);
-       Brake();
-       delay(100);
-    }
-    else if (wallGap == LOW){
-       Drive(medSpeed);
-       Brake();
-       delay(100);
-    }
-    */
-     if (currentDistance < 10){
-      //turn right
-      if(turn1){
-        turn1 = false;
-        turn2 = true;
-        ShiftRight(medSpeed,false);
-        Serial.print("Turn 1");
-        
-      }
-      else if(turn2){
-        //turn right
-        turn2 = false;
-        turn3 = true;
-        ShiftRight(medSpeed,false);
-        Serial.print("Turn 2");
-      }
-      else if(turn3){
-        //turn left
-        turn3 = false;
-        turn2 = false;
-        ShiftLeft(medSpeed,false);
-        Serial.print("Turn 3");
-      }
-    }
-    
-    
-    
-    
 
-
+    long currentDistance = ping();    //updating Ultra sonic sensor range detection 
+     IR_Pin = digitalRead(leftWallPin_12); //reading IR sensor to detect left walls
+     //ultrasonic range is 14cm, varaible as motors have latency and can overshoot
+      if (currentDistance < 14){
+          Brake();
+          if(turns == 1){//first maze turn
+              Serial.println("SHIFTING LEFT");
+              ShiftRight(lowSpeed);
+              ShiftRight(lowSpeed);
+          }
+          else if(turns == 2){//second maze turn
+              Serial.println("SHIFTING RIGHT");
+              ShiftRight(lowSpeed);
+              ShiftRight(lowSpeed);
+          }
+          else if(turns == 3){//third maze turn
+              Serial.println("SHIFTING RIGHT");
+              ShiftLeft(lowSpeed);
+              ShiftLeft(lowSpeed);
+          }     
+      turns = turns + 1; // increment the turn count, no more turns will be taken, will simply drive staight
+    }
+    Drive(lowSpeed);
 
 }//end loop
 
@@ -93,43 +65,52 @@ void loop() {
 
 
 void Drive(int speed){
-    //Serial.println("DRIVING");
+     //veering funciton used in the drive function as it should veer instead of drive if need be
+     //ultrasonic detection was getting lagged with too many drive (veer, turn, drive, etc) functions in the loop 
+     if (IR_Pin == LOW){
+      //LEFT WALL DETECTION
+      Serial.println("VEERING RIGHT");
+      veerRight(lowSpeed);
+      IR_Pin = HIGH;
+     }
+//code below is required to drive the car straight operating both motors
     LeftMotor->setSpeed(speed);
     RightMotor->setSpeed(speed);
     LeftMotor->run(FORWARD);
     RightMotor->run(FORWARD);
-    delay(100);
+    delay(80);
 }
 
-void ShiftLeft(int speed,bool dir){
-    //Serial.println("SHIFTING LEFT");
+//function to slightly turn the vehicle (only operates a single motor)
+//used as counteraction to the single IR sensor on left side of vehicle
+void veerRight(int speed){
+    //Speed is hardcoded here as detection distance was adjusted on the sensor itself, had to fine tune
+    RightMotor->setSpeed(35);
+    RightMotor->run(BACKWARD);
+    delay(275);
+}
+
+//function to turn the vehicle left (operates both motors)
+void ShiftLeft(int speed){
     RightMotor->setSpeed(speed);
-    if(dir){
-      RightMotor->run(FORWARD);
-    }
-    else{
-      LeftMotor->run(BACKWARD);
-      LeftMotor->setSpeed(speed*2);
-    }
-    delay(200);
+    LeftMotor->setSpeed(speed*2);
+    RightMotor->run(FORWARD);
+    LeftMotor->run(BACKWARD);
+    delay(430);
     Brake();
 }
 
-void ShiftRight(int speed,bool dir){
-    //Serial.println("SHIFTING RIGHT");
+//function to turn the vehicle right (operates both motors)
+void ShiftRight(int speed){
+    RightMotor->setSpeed(speed*2);
     LeftMotor->setSpeed(speed);
-    if(dir){
-      LeftMotor->run(FORWARD);
-    }
-    else{
-      RightMotor->run(BACKWARD);  
-      RightMotor->setSpeed(speed*2);  
-    }
-    delay(200);
+    RightMotor->run(BACKWARD);
+    LeftMotor->run(FORWARD);
+    delay(410);
     Brake();
-    }
+}
 
-
+//function to brake the vehicle (release and stop driving the motors)
 void Brake(){
     //Serial.println("BRAKING");
     LeftMotor->run(RELEASE);
@@ -137,8 +118,10 @@ void Brake(){
     }
     
 
+//ultrasonic detection function
+//returns distance in Cm
+long ping(){
 
-long ping(){//returns distance in Cm
   
     digitalWrite(trigPin_10, LOW);
     delayMicroseconds(2);
@@ -158,7 +141,5 @@ long ping(){//returns distance in Cm
     return distance;
     
 }
-
-
 
 
